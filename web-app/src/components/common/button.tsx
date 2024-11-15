c3c96646f8bd9ce454c4627e27e49a7cbe604228
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-11-15 12:11:27 +0100

    Update button.tsx

---
+++ web-app/src/components/common/button.tsx
@@ -0,0 +1,46 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface ButtonProps {
+  title?: string;
+  textColor?: string;
+  backgroundColor?: string;
+  image?: string;
+  buttonIcon?: IconProp;
+  roundness?: string;
+  width?: string;
+  height?: string;
+  onClickFunction: () => void;
+}
+
+const CustomButton: React.FC<ButtonProps> = ({
+  title,
+  textColor,
+  backgroundColor,
+  image,
+  buttonIcon,
+  roundness,
+  width,
+  height,
+  onClickFunction,
+}) => {
+  return (
+    <button
+      onClick={onClickFunction}
+      className="hover:scale-105 duration-300 p-4 flex items-center justify-center cursor-pointer"
+      style={{
+        color: textColor || "white",
+        backgroundColor: backgroundColor || "black",
+        borderRadius: roundness || "5px",
+        width: width || "2rem",
+        height: height || "2rem",
+      }}
+    >
+      {image && <img src={image} alt="button-icon" className="w-16" />}
+      {buttonIcon && <FontAwesomeIcon icon={buttonIcon} />}
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
